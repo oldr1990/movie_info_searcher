a72899d857169a8eb45c6b563b35f97bdf4e3855
oldr1990 <sashik4281@gmail.com>
2022-04-27 22:20:40 +0300

    add poster and title

--- lib/ui/details_screen.dart
+++ lib/ui/details_screen.dart
@@ -1,4 +1,5 @@
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -13,10 +14,32 @@ class DetailScreen extends StatelessWidget{
   Widget build(BuildContext context) {
    return Scaffold(
      appBar: AppBar(
-       title: const Text("Movie Info Searcher"),
+       title:  Text(data.title!),
        elevation: 1,
      ),
-     body: const Text("Movie Info Searcher"),
+     body: Container(
+       padding: const EdgeInsets.all(16.0),
+       child: ListView(
+         children: [
+           poster(data.poster!),
+         ],
+       ) ,
+     ),
    );
+  }
+
+  Widget poster(url){
+    return Center(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: CachedNetworkImage(
+          imageUrl: url,
+          fit: BoxFit.fill,
+          width: 300,
+          height: 446,
+        ),
+      ),
+    );
+
   }
 }
